228e23ff8032e1065aa2c2231e61b16c9bd39719
fletch8 <stampsfj04@gmail.com>
2021-03-26 12:10:16 -0400

    working on models

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Card(models.Model):
+    name = models.CharField(max_length=50)
+    image_url = models.CharField()
+
+    def __str__(self):
+        return "cards model class"
